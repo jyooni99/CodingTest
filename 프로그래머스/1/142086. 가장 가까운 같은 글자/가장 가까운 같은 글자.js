a310c90a9dc36b310cbd5ce01f02085afbb6bafc
JYOON <112847202+jyooni99@users.noreply.github.com>
2024-09-05 20:44:18 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.73 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,14 +1,12 @@
 function solution(s) {
-    let strArr = s.split('');
-    let answer = [];
-    
-    strArr.forEach((char, idx) => {
-        if(idx === 0) answer.push(-1);
-        else{
-            let lastIdx = strArr.lastIndexOf(char, idx - 1);
-            answer.push(lastIdx === -1 ? -1 : idx - lastIdx);
-        }
-    })
+	let strArr = s.split('');
+	let answer = [];
+	
+	strArr.forEach((char, idx) => {
+		let lastIdx = strArr.lastIndexOf(char, idx - 1);
+		answer.push(idx === 0 || lastIdx === -1 ? -1 : idx - lastIdx); 
+	})
     
     return answer;
+	
 }
