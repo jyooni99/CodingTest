a27557cfce47ba219b9582074459fa1f184e957f
JYOON <112847202+jyooni99@users.noreply.github.com>
2024-09-10 10:22:15 +0900

    [level 0] Title: 평행, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.js
+++ 프로그래머스/0/120875. 평행/평행.js
@@ -3,11 +3,11 @@ function solution(dots) {
     
     let [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots;
     
-    if(Math.abs((y2 - y1) / (x2 - x1)) === Math.abs((y4 - y3) / (x4 - x3))){
+    if((y2 - y1) / (x2 - x1) === (y4 - y3) / (x4 - x3)){
         return 1;
-    }else if (Math.abs((y3 - y1) / (x3 - x1)) === Math.abs((y2 - y4) - (x2 - x4))){
+    }else if ((y3 - y1) / (x3 - x1) === (y2 - y4) / (x2 - x4)){
         return 1;
-    }else if (Math.abs((y4 - y1) / (y4 - y1)) === Math.abs((y3 - y2) / (x3 - x2))){
+    }else if ((y4 - y1) / (y4 - y1) === (y3 - y2) / (x3 - x2)){
         return 1;
     }else {
         return 0;
