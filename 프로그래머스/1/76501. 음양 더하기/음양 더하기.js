22e15213b30b4403d0aca1932850f16f44167ecf
JYOON <112847202+jyooni99@users.noreply.github.com>
2024-09-15 22:20:04 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,3 +1,3 @@
 function solution(absolutes, signs) {
-    return absolutes.reduce((acc, num, idx) => signs[idx] === true ? acc += num : acc -= num, 0)
+    return absolutes.reduce((acc, num, idx) => signs[idx] ? acc += num : acc -= num, 0)
 }
