785bf629dd827b63c3696a8a80e52a9c495fbb2f
JYOON <112847202+jyooni99@users.noreply.github.com>
2024-09-16 21:24:20 +0900

    [level 1] Title: 하샤드 수, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,12 @@
+function solution(x) {
+    
+    let sum = 0;
+    let num = x;
+    
+    while(num > 0){
+        sum += num % 10;
+        num = Math.floor(num / 10);
+    }
+
+    return x % sum === 0  ? true : false;
+}
