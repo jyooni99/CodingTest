edfe08b7e8b7838f249fd218acddf5cf43ec1339
JYOON <112847202+jyooni99@users.noreply.github.com>
2024-09-23 11:53:14 +0900

    [level 2] Title: H-Index, Time: 0.70 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,10 @@
+function solution(citations) {
+    const sorted = citations.sort((a, b) => b - a);
+    let h = 0;
+    
+    for(let i = 0; i < sorted.length; i++){
+        if(sorted[i] >= i + 1) h++;
+    }
+    
+    return h;
+}
