1f28479badd82bbebe71618ce6574f7f1b4f263a
JYOON <112847202+jyooni99@users.noreply.github.com>
2024-10-01 15:55:24 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 45.73 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,5 +1,16 @@
 function solution(arr){
-    let answer = arr.filter((val, idx) => idx === 0 || arr[idx - 1] !== val)
+    const stack = [arr[0]];
+    const len = arr.length;
     
-    return answer;
+    for(let i = 1; i < len; i++){
+        if(arr[i] !== arr[i - 1]){
+            stack.push(arr[i]);
+        }
+    }
+    return stack;
 }
+
+// filter 사용
+// 메모리: 83 MB, 시간: 51.03 ms / 정확성: 71.9 효율성: 28.1
+// return arr.filter((val,index) => val !== arr[index+1]);
+
