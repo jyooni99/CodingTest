78df1fd40a6656e01072c9261563b6b0919b2d84
JYOON <112847202+jyooni99@users.noreply.github.com>
2024-10-02 13:31:23 +0900

    [level 2] Title: 더 맵게, Time: 508.41 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.js
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -1,69 +1,66 @@
-class MinHeap{
-    constructor(){
+class MinHeap {
+    constructor() {
         this.heap = [null];
     }
-    
-    size(){
-        return this.heap.length;
+
+    size() {
+        return this.heap.length - 1; // 실제 요소의 개수를 반환
     }
-    
-    push(val){
+
+    push(val) {
         this.heap.push(val);
         this.bubbleUp();
     }
-    
-    bubbleUp(){
-        let childIdx = this.size() - 1;
+
+    bubbleUp() {
+        let childIdx = this.size();
         
-        while(childIdx > 1){
+        while (childIdx > 1) {
             let parentIdx = Math.floor(childIdx / 2);
             
-            if(this.heap[childIdx] >= this.heap[parentIdx]) break;
+            if (this.heap[childIdx] >= this.heap[parentIdx]) break;
             [this.heap[childIdx], this.heap[parentIdx]] = [this.heap[parentIdx], this.heap[childIdx]];
             childIdx = parentIdx;
         }
     }
-    
-    pop(){
-        if(this.size() === 1) return null;
+
+    pop() {
+        if (this.size() === 0) return null;
         let min = this.heap[1];
         let current = this.heap.pop();
         
-        if(this.size() > 1){
+        if (this.size() > 0) {
             this.heap[1] = current;
             this.sinkDown();
         }
         
         return min;
     }
-    
-    sinkDown(){
+
+    sinkDown() {
         let idx = 1;
         let ele = this.heap[idx];
         let len = this.size();
         
-        while(true){
+        while (true) {
             let leftIdx = 2 * idx;
             let rightIdx = 2 * idx + 1;
             let leftEle, rightEle;
             let swap = null;
             
-            if(leftIdx < len){
+            if (leftIdx <= len) {
                 leftEle = this.heap[leftIdx];
-                if(leftEle < ele) swap = leftIdx;
+                if (leftEle < ele) swap = leftIdx;
             }
             
-            if(rightIdx < len){
+            if (rightIdx <= len) {
                 rightEle = this.heap[rightIdx];
-                if(
-                    (swap === null && rightEle < ele) ||
-                    (swap !== null && rightEle < leftEle)
-                ){
+                if ((swap === null && rightEle < ele) || (swap !== null && rightEle < leftEle)) {
                     swap = rightIdx;
                 }
             }
             
-            if(swap === null) break;
+            if (swap === null) break;
             
             this.heap[idx] = this.heap[swap];
             idx = swap;
@@ -72,27 +69,22 @@ class MinHeap{
     }
 }
 
-
 function solution(scoville, K) {
     let minHeap = new MinHeap();
-    let len = scoville.length;
     let mixedCnt = 0;
     
-    //힙에 추가
-    for(let scov of scoville){
+    // 힙에 추가
+    for (let scov of scoville) {
         minHeap.push(scov);
     }
     
     // 가장 작은 스코빌지수가 K 이상이면 반복 중단
-    while(minHeap.size() > 2 && minHeap.heap[1] < K){
-        
+    while (minHeap.size() > 1 && minHeap.heap[1] < K) {
         let first = minHeap.pop();
         let second = minHeap.pop() * 2;
-        
         minHeap.push(first + second);
         mixedCnt++;
     }
     
-        
     return minHeap.heap[1] >= K ? mixedCnt : -1;
 }
