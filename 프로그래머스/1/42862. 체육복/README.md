fc7315565dde1000251a22ccab77dc8da0047864
JIYOON <112847202+jyooni99@users.noreply.github.com>
2024-10-06 20:25:44 +0900

    [level 1] Title: 체육복, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.23 ms
+메모리: 33.6 MB, 시간: 0.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 05일 21:17:49
+2024년 10월 06일 20:25:41
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,21 +1,21 @@
 function solution(n, lost, reserve) {
-    // 여벌이 있지만 도난당한 학생을 제거
-    let actualLost = lost.filter(l => !reserve.includes(l));
-    let actualReserve = reserve.filter(r => !lost.includes(r));
-
-    // 체육복을 빌려주는 과정
-    actualLost.sort((a, b) => a - b);  // lost 학생들을 오름차순으로 정렬
-    actualReserve.sort((a, b) => a - b);  // reserve 학생들도 정렬
+    // 여벌 체육복을 가져온 학생이 도난 당함 -> lost와 reserve에서 제거
+    let lostArr = lost.filter(student => !reserve.includes(student));
+    let reserveArr = reserve.filter(student => !lost.includes(student));
     
-    for (let i = 0; i < actualReserve.length; i++) {
-        const borrower = actualReserve[i];
-        const lendIndex = actualLost.findIndex(l => l === borrower - 1 || l === borrower + 1);
-        
-        if (lendIndex !== -1) {
-            actualLost.splice(lendIndex, 1); // 빌려준 학생은 제거
+    //정렬
+    lostArr = lostArr.sort((a, b) => a - b);
+    reserveArr = reserveArr.sort((a, b) => a - b);
+    
+    // 도난 당한 학생에게 빌려주기
+    for(let i = 0; i < reserveArr.length; i++){
+        let reserver = reserveArr[i];
+        let index = lostArr.findIndex(num => num === reserver - 1 || num === reserver + 1);
+
+        if(index !== -1){
+            lostArr.splice(index, 1);
         }
     }
-
-    // 최종 참여 학생 수는 전체 학생 수에서 여전히 체육복이 없는 학생을 제외한 값
-    return n - actualLost.length;
+    
+    return n - lostArr.length;
 }
