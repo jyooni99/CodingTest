f929ae23f51d67b99cf83d2d4de9362034b58ec6
JIYOON <112847202+jyooni99@users.noreply.github.com>
2024-10-12 22:42:54 +0900

    [level 2] Title: 할인 행사, Time: 20.21 ms, Memory: 39.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,43 @@
+function solution(want, number, discount) {
+    let count = 0;
+    
+    let wantMap = new Map();
+    want.forEach((product, i) => {
+        wantMap.set(product, number[i]);
+    });
+    
+    let discountMap = new Map();
+    for(let i = 0; i < 10; i++){
+        let product = discount[i];
+        discountMap.set(product, (discountMap.get(product) || 0 ) + 1);
+    };
+    
+    // 상품 검사하기
+    function isMatch(){
+        for(let product of want){
+            if(wantMap.get(product) !== discountMap.get(product)){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    // 조건 확인하기
+    for(let i = 10; i <= discount.length; i++){
+        if(isMatch()){
+            count++;
+        }
+        
+        if(i < discount.length){
+            discountMap.set(discount[i], (discountMap.get(discount[i]) || 0) + 1);
+            
+            discountMap.set(discount[i - 10], (discountMap.get(discount[i - 10]) - 1));
+
+            if(discountMap.get(discount[i - 10]) === 0){
+                discountMap.delete(discount[i - 10]);
+            }   
+        }
+    }
+    
+    return count;
+}
