f5e43f81afeacf3626e7dc27ee4e382eb740cc22
JIYOON <112847202+jyooni99@users.noreply.github.com>
2024-10-16 17:03:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 50.60 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,16 +1,17 @@
 function solution(participant, completion) {
-    let map = new Map;
-    
+    const map = new Map();
+
     participant.forEach(name => {
         map.set(name, (map.get(name) || 0) + 1);
-    })
-    
+    });
+
     completion.forEach(name => {
-        map.set(name, (map.get(name)) - 1);
-        if(map.get(name) === 0){
-            map.delete(name);
+        map.set(name, map.get(name) - 1);
+    });
+
+    for (let [name, count] of map) {
+        if (count > 0) {
+            return name;
         }
-    })
-    
-    return map.keys().next().value;
+    }
 }
