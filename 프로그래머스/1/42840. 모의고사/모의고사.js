9f5d3abdee601ba44167ceb811b95e6a016f426c
JIYOON <112847202+jyooni99@users.noreply.github.com>
2024-10-18 16:46:37 +0900

    [level 1] Title: 모의고사, Time: 3.26 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,18 +1,27 @@
 function solution(answers) {
-    let count = [0, 0, 0];
     let [s1, s2, s3] = ['12345', '21232425', '3311224455'];
+    let students = {1: 0, 2: 0, 3: 0};
+    let answer = [];
     
     answers.forEach((answer, idx) => {
         answer = answer.toString();
-        if(s1[idx % s1.length] === answer) count[0]++;
-        if(s2[idx % s2.length] === answer) count[1]++;
-        if(s3[idx % s3.length] === answer) count[2]++;
-    });
+        
+        if(s1[idx % s1.length] === answer) students[1]++;
+        if(s2[idx % s2.length] === answer) students[2]++;
+        if(s3[idx % s3.length] === answer) students[3]++;
+    })
     
-    // 최대 점수 반환
-    let maxScore = Math.max(...count);
     
-    return count
-        .map((score, idx) => maxScore === score ? idx + 1 : null) // 최대 점수만 남기고 Null로 변환
-        .filter(score => score !== null); // null 제거
+    //최대 점수 찾기
+   let max = Math.max(...Object.values(students));
+    
+    
+    // 최대 점수인 학생 찾기
+    for(let num in students){
+        if(students[num] === max){
+            answer.push(parseInt(num));
+        }
+    }
+    
+    return answer;
 }
