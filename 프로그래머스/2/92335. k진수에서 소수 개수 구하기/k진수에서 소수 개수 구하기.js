673fedf73895af9f49216bab4f47ede21ac8ee5d
JIYOON <112847202+jyooni99@users.noreply.github.com>
2024-11-13 14:17:50 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 10.08 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,5 +1,5 @@
 function isPrime(n){
-    if(n === 1) return false;
+    if(n <= 1) return false;
     
     for(let i = 2; i <= Math.sqrt(n); i++){
         if(n % i === 0) return false;
@@ -27,10 +27,7 @@ function solution(n, k) {
     const primes = convertBase(n, k).split('0');
     let count = 0;
     
-    console.log(primes);
-    
     for(let prime of primes){
-        if(prime === '') continue;
         isPrime(Number(prime)) ? count++ : count
     }
     
