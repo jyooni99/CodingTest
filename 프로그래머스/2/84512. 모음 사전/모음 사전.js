d08f80b9d9574b170638db5006bb8c020a10399a
JIYOON <112847202+jyooni99@users.noreply.github.com>
2024-11-25 12:00:04 +0900

    [level 2] Title: 모음 사전, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,13 +1,4 @@
 function solution(word) {
-    const vowel = {"A":0, "E":1, "I":2, "O":3, "U":4};
-    const num = [781, 156, 31, 6, 1];
-    let answer = 0;
-    let key;
-    
-    for (let i = 0; i < word.length; i++){
-        key = word[i];
-        answer += vowel[key] * num[i] + 1;
-    }
-    
+    var answer = 0;
     return answer;
 }
