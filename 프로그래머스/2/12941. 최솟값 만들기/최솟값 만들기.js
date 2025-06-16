8b89bb64c548133f7330b518b5e1f2e569920db2
JIYOON <112847202+jyooni99@users.noreply.github.com>
2025-06-17 02:54:45 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.78 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -2,5 +2,5 @@ function solution(A,B){
     A = A.sort((a, b) => a - b);
     B = B.sort((a, b) => b - a);
     
-    return A.reduce((acc, v, i) => acc += v * B[i], 0);
+    return A.reduce((acc, cur, idx) => acc += cur * B[idx],0)
 }
