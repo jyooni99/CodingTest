a9e8d0fe284a07d6ed445d75d98dcddb74ed3cd2
JIYOON <112847202+jyooni99@users.noreply.github.com>
2025-06-18 00:52:59 +0900

    [level 1] Title: 숫자 짝꿍, Time: 560.40 ms, Memory: 194 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -2,7 +2,7 @@ function solution(X, Y) {
     const countX = Array.from({length:10}, () => 0);
     const countY = Array.from({length:10}, () => 0);
     
-    // 각각 숫자의 빈도를 계산
+    // 각각 숫자의 출현 빈도를 계산
     for(const num of X){
         countX[num]++;
     }
@@ -11,12 +11,14 @@ function solution(X, Y) {
         countY[num]++;
     }
     
-    // 공통으로 나타나는 숫자 카운트
+    // X와 Y에 공통으로 나타나는 숫자 카운트
     const countMatch = [];
     
     for (let i = 9; i >= 0; i--){
-        const match = Math.min(countX[i], countY[i]);
+        // X에 1이 2개, Y에 1이 3개면 짝지을 수 있는 1은 2개
+        const match = Math.min(countX[i], countY[i]); 
         
+        // 공통된 숫자가 있다면, 공통으로 나타나는 횟수만큼 i를 배열에 추가
         if (match !== 0){
             for (let j = 0; j < match; j++){
                countMatch.push(i);
@@ -24,9 +26,7 @@ function solution(X, Y) {
         }
     }
     
-    console.log(countMatch)
-    
-    
+    // 조건에 따라 값 반환
     if (countMatch.length === 0){
         return '-1'
     }else{
